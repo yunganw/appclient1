aa763d59e5842b4b29d97cdd22130c3cea3d6277
Yungang <yunganw@amazon.com>
2022-08-23 14:06:17 +1000

    init with amplify updated version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {Amplify, Auth, Hub} from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+const UnauthedUser = () => {
+    return (
+        <div>
+            <h1>App Client1</h1>
+            You have not signed in yet.<p/>
+            <button onClick={() => Auth.federatedSignIn()}>
+                Open Hosted UI
+            </button>
+        </div>
+    );
+};
+
+const AuthedUser = (user) => {
+  console.log ('user:', user);
+    return (
+        <div>
+          <h1> Hi {user.user.username}</h1>
+          You've signed in to AppClient1 <p/>
+            <button onClick={() => Auth.signOut()}>
+                Sign Out
+            </button>
+        </div>
+    );
+};
+
+export default function App() {
+    const [user, setUser] = useState(null);
+    const [customState, setCustomState] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = Hub.listen('auth', ({payload: {event, data}}) => {
+            switch (event) {
+                case 'signIn':
+                    setUser(data);
+                    break;
+                case 'signOut':
+                    setUser(null);
+                    break;
+                case 'customOAuthState':
+                    setCustomState(data);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        Auth.currentAuthenticatedUser()
+            .then((currentUser) => setUser(currentUser))
+            .catch(() => console.log('Not signed in'));
+
+        return unsubscribe;
+    }, []);
+
+    return user ? <AuthedUser user={user}/> : <UnauthedUser />;
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Authenticator} from '@aws-amplify/ui-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Authenticator.Provider>
+            <App />
+        </Authenticator.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
