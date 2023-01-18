ef1b5c399755d432ffcd8b7617faa0850247b65b
Yungang <yunganw@amazon.com>
2023-01-18 16:03:27 +1100

    add json view for user info

--- src/App.js
+++ src/App.js
@@ -1,78 +1,88 @@
 import React, {useEffect, useState} from 'react';
 import {Amplify, Auth, Hub} from 'aws-amplify';
 import awsconfig from './aws-exports';
+import ReactJson from "react-json-view";
 
 Amplify.configure(awsconfig);
 
-
 async function globalSignOut() {
-    
-    try {
-        await Auth.signOut();
-        await Auth.signOut({ global: true });
-    } catch (error) {
-        console.log('error signing out: ', error);
-    }
+  try {
+    await Auth.signOut();
+    await Auth.signOut({global: true});
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
 }
 
 const UnauthedUser = () => {
-    return (
-        <div>
-            <h1>Application 1</h1>
-            You have not signed in yet.<p/>
-            <button onClick={() => Auth.federatedSignIn()}>
-                Open Hosted UI
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Application 1</h1>
+      You have not signed in yet.
+      <p />
+      <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+    </div>
+  );
 };
 
 const AuthedUser = (user) => {
-  console.log ('user:', user);
-    return (
-        <div>
-          <h1> Hi {user.user.username}</h1>
-          You've signed in to Application 1 <p/>
-            <p/>
-            <button onClick={() => globalSignOut()}>
-                Sign Out
-            </button>
-        </div>
-    );
+  console.log('user:', user);
+  return (
+    <div>
+      <h1> Hi {user.user.username}</h1>
+      <p> User info details: </p>
+      <ReactJson src={user.user.attributes} />
+      You've signed in to Application 1 <p />
+      <p />
+      <button onClick={() => globalSignOut()}>Sign Out</button>
+    </div>
+  );
 };
 
 const Loading = () => {
-    return (<div id="loading"></div>);
-}
+  return <div id='loading'>loading</div>;
+};
 
 export default function App() {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const unsubscribe = Hub.listen('auth', ({payload: {event, data}}) => {
-            switch (event) {
-                case 'signIn':
-                    setLoading(false);
-                    setUser(data);
-                    break;
-                case 'signOut':
-                    setLoading(false);
-                    setUser(null);
-                    break;
-                default:
-                    break;
-            }
-        });
+  useEffect(() => {
+    const unsubscribe = Hub.listen('auth', ({payload: {event, data}}) => {
+      switch (event) {
+        case 'signIn':
+          setLoading(false);
+          setUser(data);
+          break;
+        case 'signOut':
+          setLoading(false);
+          setUser(null);
+          break;
+        default:
+          break;
+      }
+    });
 
-        setLoading(true);
-        Auth.currentAuthenticatedUser()
-            .then((currentUser) => {setUser(currentUser); setLoading(false)})
-            .catch(() => {setLoading(false);console.log('Not signed in')});
+    setLoading(true);
+    Auth.currentAuthenticatedUser()
+      .then((currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        console.log('Not signed in');
+      });
 
-        return unsubscribe;
-    }, []);
+    return unsubscribe;
+  }, []);
 
-    // return <Loading />;
-    return loading ? <Loading /> : (user ? <AuthedUser user={user}/> : <UnauthedUser />);
+  // return <Loading />;
+  return loading ? (
+    <Loading />
+  ) : user ? (
+    <AuthedUser user={user} />
+  ) : (
+    <UnauthedUser />
+  );
 }
